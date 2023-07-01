20a86d8d24eae34eef0d8238cb202c460e2b1400
AshGw <AshrefGw@proton.me>
2023-07-01 17:31:10 +0100

    refactor Cli enum

---
+++ src/cli.rs
@@ -0,0 +1,34 @@
+use structopt::StructOpt;
+use jsonwebtoken::Algorithm;
+
+#[derive(StructOpt, Debug)]
+pub enum Cli {
+    #[structopt(name = "encode")]
+    Encode {
+        #[structopt(long = "email")]
+        email: String,
+
+        #[structopt(long = "scopes")]
+        scopes: String,
+
+        #[structopt(long = "algorithm", default_value = "HS256")]
+        algorithm: Algorithm,
+
+        #[structopt(long = "secret")]
+        secret: String,
+
+        #[structopt(long = "expiration-days", default_value = "30")]
+        expiration_days: i64,
+    },
+    #[structopt(name = "decode")]
+    Decode {
+        #[structopt(long = "token")]
+        token: String,
+
+        #[structopt(long = "algorithm", default_value = "HS256")]
+        algorithm: Algorithm,
+
+        #[structopt(long = "secret")]
+        secret: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,21 @@
-use std::collections::HashMap;
-use structopt::StructOpt;
-use crate::jaats::{jwt_encode, jwt_decode};
-use jsonwebtoken::Algorithm;
 
 mod models;
 mod jaats;
-
-#[derive(StructOpt, Debug)]
-enum Cli {
-    #[structopt(name = "encode")]
-    Encode {
-        #[structopt(long = "email")]
-        email: String,
-
-        #[structopt(long = "scopes")]
-        scopes: String,
-
-        #[structopt(long = "algorithm", default_value = "HS256")]
-        algorithm: Algorithm,
-
-        #[structopt(long = "secret")]
-        secret: String,
-
-        #[structopt(long = "expiration-days", default_value = "30")]
-        expiration_days: i64,
-    },
-    #[structopt(name = "decode")]
-    Decode {
-        #[structopt(long = "token")]
-        token: String,
-
-        #[structopt(long = "algorithm", default_value = "HS256")]
-        algorithm: Algorithm,
-
-        #[structopt(long = "secret")]
-        secret: String,
-    },
-}
-
+mod cli;
+use structopt::StructOpt;
+use std::collections::HashMap;
+use crate::jaats::{jwt_encode, jwt_decode};
+use crate::cli::Cli;
 
 fn main() {
-    let args = Cli::from_args();
+    let args: Cli = Cli::from_args();
 
     match args {
         Cli::Encode { email, scopes, algorithm, secret, expiration_days } => {
             let scope: HashMap<String, String> = scopes
                 .split(',')
-                .map(|pair| {
-                    let mut iter = pair.split('=');
+                .map(|pair: &str| {
+                    let mut iter: std::str::Split<'_, char> = pair.split('=');
                     (iter.next().unwrap().to_string(), iter.next().unwrap_or("").trim().to_string())
                 })
                 .collect();
